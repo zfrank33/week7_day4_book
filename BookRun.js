ef82fcee2059dee1f4f7ea1ae238a1070a1dfa63
Francisco Guadarrama <107962016+zfrank33@users.noreply.github.com>
2022-08-12 10:06:52 -0500

    Add files via upload

---
+++ BookRun.js
@@ -0,0 +1,91 @@
+// import { apiClientNoAuth, apiClientTokenAuth } from "./client.js";
+import  BookApi from './BookApi.js';
+
+
+const get = async () =>{
+    let error
+    let res
+
+    const response = await BookApi().get('/login')
+    if (response.ok){
+        res = response.data
+        console.log(res)
+    }else{
+        error = "An Unexpected Error Occurred.  Please Try Again Later"  
+        console.log(error)
+
+    }
+    return {
+        error,
+        res
+    }
+}
+
+const getBook = async () =>{
+    let error
+    let res
+
+    const response = await BookApi().get('/book')
+    if (response.ok){
+        res = response.data
+        console.log(res)
+
+    }else{
+        error = "An Unexpected Error Occurred.  Please Try Again Later"  
+        console.log(error)
+
+    }
+    return {
+        error,
+        res
+    }
+}
+
+const post = async (catName) =>{
+    let error
+    const response = await BookApi().post('/user', {name: catName})
+    if (!response.ok){
+        error = "An Unexpected Error Occurred.  Please Try Again Later" 
+        console.log(error)
+
+    }
+    return {
+        error
+    }
+}
+
+const put = async ( id, catName) =>{
+    let error
+    const response = await BookApi().put('/user'+'/'+id, {name: catName})
+    if (!response.ok){
+        error = "An Unexpected Error Occurred.  Please Try Again Later"  
+        console.log(error)
+
+    }
+    return {
+        error
+    }
+}
+
+const del = async (id) =>{
+    let error
+    const response = await BookApi().delete('/user'+'/'+id)
+    if (!response.ok){
+        error = "An Unexpected Error Occurred.  Please Try Again Later"  
+        console.log(error)
+
+    }
+    return {
+        error
+    }
+}
+
+const BookRun={
+    get,
+    getBook,
+    post,
+    put,
+    del
+}
+
+export default BookRun
